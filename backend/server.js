607a1642c81bb30a80c7f458f4d7d46d3065d938
t.trung2303@gmail.com <T.trung2303@gmail.com>
2024-06-18 12:53:47 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js"
+import connectMongoDB from "./db/connectMongoDB.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () =>{
+    console.log(`Server is now running in port ${PORT}`);
+    connectMongoDB();
+})
