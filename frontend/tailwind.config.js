abfc4606abc6cdd1b94d8c7470315a92cf8be9f5
t.trung2303@gmail.com <T.trung2303@gmail.com>
2024-07-13 16:20:40 +0700

    change theme to light

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,23 +1,25 @@
 import daisyui from "daisyui";
 import daisyUIThemes from "daisyui/src/theming/themes";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [daisyui],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
 
-	daisyui: {
-		themes: [
-			"light",
-			{
-				black: {
-					...daisyUIThemes["black"],
-					primary: "rgb(29, 155, 240)",
-					secondary: "rgb(24, 24, 24)",
-				},
-			},
-		],
-	},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          ...daisyUIThemes["light"],
+          primary: "#ffffff",
+          secondary: "#ffffff",
+          "primary-content": "#000000", // Text color on primary background
+          "secondary-content": "#000000", // Text color on secondary background
+        },
+      },
+    ],
+  },
 };
