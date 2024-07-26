b05b649f9f5820e56d65b5680c890060fc295337
t.trung2303@gmail.com <T.trung2303@gmail.com>
2024-07-26 14:18:17 +0700

    try

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-				changeOrigin: true,
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'build'  // Thay đổi thư mục đầu ra thành 'build'
+  }
 });
