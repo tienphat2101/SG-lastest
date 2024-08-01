fdce19f84b7aa45692290959c127a6fe4abe4646
DarkRin0 <phongcaovo@gmail.com>
2024-08-01 10:58:07 +0700

    saf

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -98,7 +98,7 @@ const Sidebar = () => {
 							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
 						>
 							<FaBatteryQuarter className='w-6 h-6' />
-							<span className='text-lg hidden md:block'>Smart-list</span>
+							<span className='text-lg hidden md:block'>Smart List</span>
 						</Link>
 					</li>
 					<li className='flex justify-center md:justify-start'>
